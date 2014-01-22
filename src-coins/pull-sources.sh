e051257f5d37ded34cbc253925251923b31900ed
hscoin <wapradio.jdr@gmail.com>
2014-01-22 20:14:59 +0100

    work

--- src-coins/pull-sources.sh
+++ src-coins/pull-sources.sh
@@ -1,46 +1,55 @@
 cd 6coin
 git pull
 cd ..
+#
 cd alphacoin
 git pull
 cd ..
+#
 cd AmericanCoin
 git pull
 cd ..
+#
 cd BBQCoin
 git pull
 cd ..
+#
 cd bitcoin
 git pull
 cd ..
+#
 cd CGB
 git pull
 cd ..
+#
 cd CHNCoin
 git pull
 cd ..
+#
 cd digitalcoin
 git pull
 cd ..
+#
 cd dogecoin
 git pull
 cd ..
+#
 cd feathercoin
 git pull
 cd ..
-cd firstcoin
-git pull
-cd ..
+#
 cd freicoin
 git pull
 cd ..
+#
 cd hashcoin
 git pull
 cd ..
+#
 cd litecoin
 git pull
 cd ..
+#
 cd royalcoin
 git pull
 cd ..
-
